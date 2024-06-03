6e822e0d2f510a2ca0c9dac13c67a9c9f3fc4a48
Madhuri <madhuri.infasta@gmail.com>
2024-06-03 12:08:00 +0530

    store and carts functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+def home():
+    return HttpResponse("hello account")
+
+def register(request):
+    pass
+    # return render(request,"accounts/login.html")
+def login(request):
+    return render(request,"accounts/login.html")
+
+
+def logout(request):
+    return render(request,"accounts/logout.html")
